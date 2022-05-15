f3472731cdad530215efb0e06f951d7f7d5cd11d
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-15 03:47:57 +0200

    Aggiunto logo background + liste dinamiche (parte superiore componente footer).

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,50 +2,18 @@
   <footer>
     <div class="top">
       <div class="container">
-        <div class="footer-list">
-          <h3>DC COMICS</h3>
-          <ul>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-          </ul>
+        <div class="lists-container">
+          <div v-for="(list, index) in lists" :key="`list-${index}`">
+            <h3>{{list.listName.toUpperCase()}}</h3>
+            <ul>
+              <li v-for="(option, index2) in list.options" :key="`option-${index2}`">
+                <a :href="option.href">{{option.optName}}</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="footer-list">
-          <h3>SHOP</h3>
-          <ul>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-          </ul>
-        </div>
-        <div class="footer-list">
-          <h3>DC</h3>
-          <ul>
-            <li>Terms Of Use</li>
-            <li>Privacy policy (New)</li>
-            <li>Ad Choices</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div class="footer-list">
-          <h3>Sites</h3>
-          <ul>
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
-          </ul>
+        <div class="logo-container">
+          <img src="../assets/img/dc-logo-bg.png" alt="">
         </div>
       </div>
     </div>
@@ -55,7 +23,133 @@
 
 <script>
 export default {
-  name: "FooterComp"
+  name: "FooterComp",
+  data(){
+    return{
+      lists: [
+        {
+          listName: "Dc Comics",
+          options: [
+            {
+              optName: "Characters",
+              href: "#"
+            },
+            {
+              optName: "Comics",
+              href: "#"
+            },
+            {
+              optName: "Movies",
+              href: "#"
+            },
+            {
+              optName: "TV",
+              href: "#"
+            },
+            {
+              optName: "Games",
+              href: "#"
+            },
+            {
+              optName: "Videos",
+              href: "#"
+            },
+            {
+              optName: "News",
+              href: "#"
+            }
+          ]
+        },
+        {
+          listName: "Shop",
+          options: [
+            {
+              optName: "Shop DC",
+              href: "#"
+            },
+            {
+              optName: "Shop DC Collectibles",
+              href: "#"
+            }
+          ]
+        },
+        {
+          listName: "Dc",
+          options: [
+            {
+              optName: "Terms Of Use",
+              href: "#"
+            },
+            {
+              optName: "Privacy policy (New)",
+              href: "#"
+            },
+            {
+              optName: "Ad Choices",
+              href: "#"
+            },
+            {
+              optName: "Advertising",
+              href: "#"
+            },
+            {
+              optName: "Jobs",
+              href: "#"
+            },
+            {
+              optName: "Subscriptions",
+              href: "#"
+            },
+            {
+              optName: "Talent Workshops",
+              href: "#"
+            },
+            {
+              optName: "CPSC Certificates",
+              href: "#"
+            },
+            {
+              optName: "Ratings",
+              href: "#"
+            },
+            {
+              optName: "Shop Help",
+              href: "#"
+            },
+            {
+              optName: "Contact Us",
+              href: "#"
+            }
+          ]
+        },
+        {
+          listName: "Sites",
+          options: [
+            {
+              optName: "DC",
+              href: "#"
+            },
+            {
+              optName: "MAD Magazine",
+              href: "#"
+            },
+            {
+              optName: "DC Kids",
+              href: "#"
+            },
+            {
+              optName: "DC Universe",
+              href: "#"
+            },
+            {
+              optName: "DC Power Visa",
+              href: "#"
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -67,12 +161,22 @@ export default {
       padding: 40px 0;
       background-image: url("../assets/img/footer-bg.jpg");
       background-size: cover;
+      overflow: hidden;
       .container{
+        justify-content: space-between;
+      }
+      .lists-container{
+        height: 100%;
+        width: 56%;
+        display: flex;
         flex-direction: column;
         align-items: flex-start;
         align-content: flex-start;
         justify-content: left;
         flex-wrap: wrap;
+      }
+      .logo-container{
+        width: 44%;
       }
       h3{
         font-size: 22px;
@@ -83,7 +187,13 @@ export default {
         font-size: 14px;
         li{
           margin-bottom: 3px;
+        }
+        a{
           color: gray;
+          text-decoration: none;
+          &:hover{
+            text-decoration: underline;
+          }
         }
       }
     }
